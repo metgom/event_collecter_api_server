8c9a2ec05c8d1d03135c0ea8e47aa603b3c7c33c
metgom <respectpyo@gmail.com>
2022-10-24 15:14:09 +0900

    add gunicorn config

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ def shutdown_event():
     close_db()
 
 
-# for debug
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# # for debug
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
