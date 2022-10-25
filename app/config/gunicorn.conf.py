9a6d41d79e6bac6932be3c189a51e5306f195b42
metgom <respectpyo@gmail.com>
2022-10-26 00:05:21 +0900

    modified gunicorn config file - log path

--- app/config/gunicorn.conf.py
+++ app/config/gunicorn.conf.py
@@ -1,7 +1,7 @@
 daemon = True
 bind = "0.0.0.0:5000"
 worker_class = "uvicorn.workers.UvicornWorker"
-accesslog = "../log/access.log"
-errorlog = "../log/error.log"
+accesslog = "./log/access.log"
+errorlog = "./log/error.log"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s %(M)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
